651ce338937df92896f069edf547e8ba97090c79
siqizheng <cn.siqizheng@gmail.com>
2024-01-15 13:30:31 +0100

    template

---
+++ frontend/src/components/PageContent/index.tsx
@@ -0,0 +1,11 @@
+import AppRoutes from "../AppRoutes/index.js";
+
+function PageContent() {
+    return (
+        // <div className="PageContent">
+        //     <AppRoutes />
+        // </div>
+        <div>pagecontent</div>
+    );
+}
+export default PageContent;
