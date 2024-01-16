a262fd32498d5c1b0a6cabb7eb10758928bc533f
siqizheng <cn.siqizheng@gmail.com>
2024-01-16 10:47:34 +0100

    change logo

--- frontend/src/components/AppHeader/index.tsx
+++ frontend/src/components/AppHeader/index.tsx
@@ -1,13 +1,13 @@
 import { Image, Typography } from "antd";
-
+import myImage from "../../assets/logo-header.jpeg";
 function AppHeader() {
 
 
     return (
         <div className="AppHeader">
             <Image
-                width={40}
-                src="https://yt3.ggpht.com/ytc/AMLnZu83ghQ28n1SqADR-RbI2BGYTrqqThAtJbfv9jcq=s176-c-k-c0x00ffffff-no-rj"
+                width={140}
+                src={myImage}
             ></Image>
             <Typography.Title>Management System</Typography.Title>
 
