8c7e41f362f98d5f973a1bf52b7422004117f5e2
siqizheng <cn.siqizheng@gmail.com>
2024-02-02 12:56:00 +0100

    send message in frontend

--- backend/src/main/java/com/example/backend/configuration/ReactRoutingForwarding.java
+++ backend/src/main/java/com/example/backend/configuration/ReactRoutingForwarding.java
@@ -3,12 +3,12 @@ package com.example.backend.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.web.reactive.resource.PathResourceResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import reactor.core.publisher.Mono;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
+
 
 @Configuration
 public class ReactRoutingForwarding implements WebMvcConfigurer {
